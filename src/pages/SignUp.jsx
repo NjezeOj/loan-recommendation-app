96d57b51deb0271b79915943bd028151e0bf475f
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-12-12 23:35:50 +0100

    SignUp Done

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,160 @@
+import * as React from 'react';
+import axios from 'axios';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import {
+  Link
+} from "react-router-dom";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" to="#">
+        Team Elevate
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignIn(props) {
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+      companyName: data.get('companyName'),
+      phoneno: data.get('phoneno'),
+      address: data.get('address')
+    });
+
+    const newCompany = {
+      companyName: data.get('companyName'),
+      email: data.get('email'),
+      password: data.get('password'),
+      phone: data.get('phoneno'),
+      address: data.get('address')
+    }
+
+    try {
+
+      await axios.post(
+        'https://elevatei4g.herokuapp.com/api/v1/company/signup',
+        newCompany,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          }
+        }
+      );
+      props.history.push('/')
+
+
+    } catch (err) {
+      alert("Email already exists")
+      console.log("error:", err.response.data.error)
+    } 
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="companyName"
+              label="Company Name"
+              name="companyName"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Company Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="phoneno"
+              label="Phone Number"
+              id="phoneno"
+              
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="address"
+              label="Address"
+              id="address"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container>
+              <Grid item>
+                  <Link to="/" variant="body2">
+                    {"Have an accunt? Login"}
+                  </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
