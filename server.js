9ccab3402b951c7701a1e4fbdba7bcb95eff10d1
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-12-13 23:07:04 +0100

    Deployment

--- server.js
+++ server.js
@@ -10,12 +10,13 @@ app.use(cors());
 
 if( process.env.NODE_ENV == 'production'){
     app.use(express.static('client/build'))
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
 }
 
 
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+
 
 app.listen(PORT, () => {
     console.log('server is running on port:' + PORT);
