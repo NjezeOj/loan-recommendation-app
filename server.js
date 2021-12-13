3ae12275665e34cd03c14d03fedd39bcf9f9f507
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-12-13 23:30:26 +0100

    Deployment

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 
 if( process.env.NODE_ENV == 'production'){
-    app.use(express.static('client/build'))
+    app.use(express.static(path.join(__dirname, 'client/build')))
+       
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
     });
 }
 
