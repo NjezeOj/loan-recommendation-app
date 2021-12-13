bb4a3bceb5a4d5c121050537c682ce770bcddc1f
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-12-13 22:13:04 +0100

    Deployment Change

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path')
 
 const app = express();
 
@@ -9,6 +10,11 @@ app.use(cors());
 if( process.env.NODE_ENV == 'production'){
     app.use(express.static('client/build'))
 }
+
+app.use(routes);
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, () => {
     console.log('server is running on port:' + PORT);
