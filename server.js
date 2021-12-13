50192ab89254cd127a6717ff3ce9721c7b8c23be
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2021-12-13 23:01:23 +0100

    Deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const routes = require("./client/src/index");
+
 
 const app = express();
 
@@ -12,9 +12,9 @@ if( process.env.NODE_ENV == 'production'){
     app.use(express.static('client/build'))
 }
 
-app.use(routes);
+
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
